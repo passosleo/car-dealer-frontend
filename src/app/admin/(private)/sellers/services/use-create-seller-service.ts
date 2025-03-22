f3d8af8e2af5200f3bcc6e3143b8cc076866f747
passosleo <leonardopassos98@gmail.com>
2025-03-22 18:10:30 -0300

    feat: add create seller feedback

--- src/app/admin/(private)/sellers/services/use-create-seller-service.ts
+++ src/app/admin/(private)/sellers/services/use-create-seller-service.ts
@@ -1,14 +1,17 @@
 import { useCustomMutate } from "@/services/hooks/use-custom-mutate";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export function useCreateSellerService() {
   const router = useRouter();
+
   const { mutate: createSeller, ...data } = useCustomMutate({
     routeName: "createSeller",
     setQueryKeys: ["createSeller"],
     invalidateQueryKeys: ["listSellers"],
     onSuccess: () => {
       router.replace("/admin/sellers");
+      toast.success("Vendedor criado com sucesso");
     },
   });
 
