8c5b3e2d99ab12dac2f6a42fff7dc5a2346ac2b4
passosleo <leonardopassos98@gmail.com>
2025-07-05 16:42:20 -0300

    feat: dashboard initial structure

--- src/app/admin/(private)/layout.tsx
+++ src/app/admin/(private)/layout.tsx
@@ -1,8 +1,9 @@
 import { AppSidebar } from "@/components/admin/app-sidebar/app-sidebar";
 import { UserProfile } from "@/components/admin/user-profile/user-profile";
+import { Button } from "@/components/ui/button";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { UserInfo } from "@/services/types";
-import { BellIcon } from "lucide-react";
+import { EyeIcon } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
 
@@ -26,17 +27,24 @@ export default async function AdminPrivateLayout({
       <div className="flex items-center justify-between bg-sidebar h-14 w-full border-b border-sidebar-border fixed top-0 z-10 pr-6">
         <SidebarTrigger className="ml-2" />
 
-        <div className="flex items-center gap-2">
-          <UserProfile userInfo={userInfo} />
+        <div className="flex items-center gap-4">
+          <Link href="/" target="_blank">
+            <Button variant="outline" className="font-normal">
+              Visualizar site
+              <EyeIcon size={18} />
+            </Button>
+          </Link>
 
           <div className="w-[1px] bg-sidebar-border rounded-full h-8" />
 
-          <Link
+          <UserProfile userInfo={userInfo} />
+
+          {/* <Link
             href="/admin/notifications"
             className="flex items-center justify-center px-2 h-10 w-10 rounded-full hover:bg-muted transition-all text-muted-foreground"
           >
             <BellIcon size={18} />
-          </Link>
+          </Link> */}
         </div>
       </div>
       <main className="w-full pt-20 px-6 h-screen flex flex-col">
